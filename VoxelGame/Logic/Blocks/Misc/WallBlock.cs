74bdccaaada1dee81099979bb72cdb6f14a4309f
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2020-07-26 14:37:58 +0200

    Moved common code from fence and wall into a base class.

--- VoxelGame/Logic/Blocks/Misc/WallBlock.cs
+++ VoxelGame/Logic/Blocks/Misc/WallBlock.cs
@@ -20,21 +20,9 @@ namespace VoxelGame.Logic.Blocks
     // e = connected east
     // s = connected south
     // w = connected west
-    public class WallBlock : Block, IConnectable
+    public class WallBlock : ConnectingBlock
     {
-        private protected uint postVertCount;
-        private protected uint extensionVertCount;
         private protected uint straightVertCount;
-
-        private protected float[] postVertices = null!;
-
-        private protected float[] northVertices = null!;
-        private protected float[] eastVertices = null!;
-        private protected float[] southVertices = null!;
-        private protected float[] westVertices = null!;
-
-        private protected int[][] textureIndices = null!;
-        private protected uint[][] indices = null!;
 
         private protected float[] extensionStraightZVertices = null!;
         private protected float[] extensionStraightXVertices = null!;
@@ -42,95 +30,34 @@ namespace VoxelGame.Logic.Blocks
         private protected int[] texIndicesStraight = null!;
         private protected uint[] indicesStraight = null!;
 
-        private protected string texture, post, extension, extensionStraight;
+        private protected string extensionStraight;
 
         public WallBlock(string name, string namedId, string texture, string post, string extension, string extensionStraight) :
             base(
                 name: name,
                 namedId,
-                isFull: false,
-                isOpaque: false,
-                renderFaceAtNonOpaques: true,
-                isSolid: true,
-                recieveCollisions: false,
-                isTrigger: false,
-                isReplaceable: false,
-                isInteractable: false,
-                new BoundingBox(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.25f, 0.5f, 0.25f)),
-                TargetBuffer.Complex)
+                texture,
+                post,
+                extension,
+                new BoundingBox(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.25f, 0.5f, 0.25f)))
         {
-            this.texture = texture;
-            this.post = post;
-            this.extension = extension;
             this.extensionStraight = extensionStraight;
         }
 
         protected override void Setup()
         {
-            BlockModel postModel = BlockModel.Load(this.post);
-            BlockModel extensionModel = BlockModel.Load(this.extension);
+            base.Setup();
+
             BlockModel extensionStraightModel = BlockModel.Load(this.extensionStraight);
-
-            postVertCount = (uint)postModel.VertexCount;
-            extensionVertCount = (uint)extensionModel.VertexCount;
             straightVertCount = (uint)extensionStraightModel.VertexCount;
-
-            postModel.ToData(out postVertices, out _, out _);
-
-            extensionModel.RotateY(0, false);
-            extensionModel.ToData(out northVertices, out _, out _);
-
-            extensionModel.RotateY(1, false);
-            extensionModel.ToData(out eastVertices, out _, out _);
-
-            extensionModel.RotateY(1, false);
-            extensionModel.ToData(out southVertices, out _, out _);
-
-            extensionModel.RotateY(1, false);
-            extensionModel.ToData(out westVertices, out _, out _);
-
-            int tex = Game.BlockTextureArray.GetTextureIndex(texture);
-
-            textureIndices = new int[5][];
-
-            for (int i = 0; i < 5; i++)
-            {
-                int[] texInd = new int[postModel.VertexCount + (i * extensionModel.VertexCount)];
-
-                for (int v = 0; v < texInd.Length; v++)
-                {
-                    texInd[v] = tex;
-                }
-
-                textureIndices[i] = texInd;
-            }
-
-            indices = new uint[5][];
-
-            for (int i = 0; i < 5; i++)
-            {
-                uint[] ind = new uint[(postModel.Quads.Length * 6) + (i * extensionModel.Quads.Length * 6)];
-
-                for (int f = 0; f < postModel.Quads.Length + (i * extensionModel.Quads.Length); f++)
-                {
-                    uint offset = (uint)(f * 4);
-
-                    ind[(f * 6) + 0] = 0 + offset;
-                    ind[(f * 6) + 1] = 2 + offset;
-                    ind[(f * 6) + 2] = 1 + offset;
-                    ind[(f * 6) + 3] = 0 + offset;
-                    ind[(f * 6) + 4] = 3 + offset;
-                    ind[(f * 6) + 5] = 2 + offset;
-                }
-
-                indices[i] = ind;
-            }
 
             extensionStraightModel.RotateY(0, false);
             extensionStraightModel.ToData(out extensionStraightZVertices, out texIndicesStraight, out indicesStraight);
 
             extensionStraightModel.RotateY(1, false);
             extensionStraightModel.ToData(out extensionStraightXVertices, out _, out _);
+
+            int tex = Game.BlockTextureArray.GetTextureIndex(texture);
 
             for (int i = 0; i < texIndicesStraight.Length; i++)
             {
@@ -212,110 +139,7 @@ namespace VoxelGame.Logic.Blocks
                 return straightVertCount;
             }
 
-            int extensions = BitHelper.CountSetBits(data & 0b1111);
-            uint vertCount = (uint)(postVertCount + (extensions * extensionVertCount));
-
-            vertices = new float[vertCount * 8];
-            textureIndices = this.textureIndices[extensions];
-            indices = this.indices[extensions];
-
-            // Combine the required vertices into one array
-            int position = 0;
-            Array.Copy(postVertices, 0, vertices, 0, postVertices.Length);
-            position += postVertices.Length;
-
-            if (north)
-            {
-                Array.Copy(northVertices, 0, vertices, position, northVertices.Length);
-                position += northVertices.Length;
-            }
-
-            if (east)
-            {
-                Array.Copy(eastVertices, 0, vertices, position, eastVertices.Length);
-                position += eastVertices.Length;
-            }
-
-            if (south)
-            {
-                Array.Copy(southVertices, 0, vertices, position, southVertices.Length);
-                position += southVertices.Length;
-            }
-
-            if (west)
-            {
-                Array.Copy(westVertices, 0, vertices, position, westVertices.Length);
-            }
-
-            tint = TintColor.None;
-            isAnimated = false;
-
-            return vertCount;
-        }
-
-        protected override bool Place(Entities.PhysicsEntity? entity, int x, int y, int z)
-        {
-            byte data = 0;
-            // Check the neighboring blocks
-            if (Game.World.GetBlock(x, y, z - 1, out _) is IConnectable north && north.IsConnetable(BlockSide.Front, x, y, z - 1))
-                data |= 0b0_1000;
-            if (Game.World.GetBlock(x + 1, y, z, out _) is IConnectable east && east.IsConnetable(BlockSide.Left, x + 1, y, z))
-                data |= 0b0_0100;
-            if (Game.World.GetBlock(x, y, z + 1, out _) is IConnectable south && south.IsConnetable(BlockSide.Back, x, y, z + 1))
-                data |= 0b0_0010;
-            if (Game.World.GetBlock(x - 1, y, z, out _) is IConnectable west && west.IsConnetable(BlockSide.Right, x - 1, y, z))
-                data |= 0b0_0001;
-
-            Game.World.SetBlock(this, data, x, y, z);
-
-            return true;
-        }
-
-        internal override void BlockUpdate(int x, int y, int z, byte data, BlockSide side)
-        {
-            byte newData = data;
-
-            switch (side)
-            {
-                case BlockSide.Back:
-
-                    newData = CheckNeighbour(x, y, z - 1, BlockSide.Front, 0b0_1000, newData);
-                    break;
-
-                case BlockSide.Right:
-
-                    newData = CheckNeighbour(x + 1, y, z, BlockSide.Left, 0b0_0100, newData);
-                    break;
-
-                case BlockSide.Front:
-
-                    newData = CheckNeighbour(x, y, z + 1, BlockSide.Back, 0b0_0010, newData);
-                    break;
-
-                case BlockSide.Left:
-
-                    newData = CheckNeighbour(x - 1, y, z, BlockSide.Right, 0b0_0001, newData);
-                    break;
-            }
-
-            if (newData != data)
-            {
-                Game.World.SetBlock(this, newData, x, y, z);
-            }
-
-            static byte CheckNeighbour(int x, int y, int z, BlockSide side, byte mask, byte newData)
-            {
-                if (Game.World.GetBlock(x, y, z, out _) is IConnectable neighbour && neighbour.IsConnetable(side, x, y, z))
-                {
-                    newData |= mask;
-                }
-                else
-                {
-                    newData = (byte)(newData & ~mask);
-                }
-
-                return newData;
-            }
+            return base.GetMesh(side, data, out vertices, out textureIndices, out indices, out tint, out isAnimated);
         }
     }
 }
