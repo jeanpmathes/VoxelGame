e7a4cb01e3d2ae4e2a3d13cb71d11b50151713cc
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2020-09-11 16:54:30 +0200

    Removed unnecessary using.

--- client/Program.cs
+++ client/Program.cs
@@ -3,7 +3,6 @@
 //	   For full license see the repository.
 // </copyright>
 // <author>pershingthesecond</author>
-using OpenToolkit.Mathematics;
 using OpenToolkit.Windowing.Desktop;
 using System;
 using Microsoft.Extensions.Logging;
