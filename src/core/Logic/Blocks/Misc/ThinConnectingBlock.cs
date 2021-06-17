5807dbb2f0c4040f0692f90458a6011ef82e773f
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-06-17 22:11:13 +0200

    Add thin connecting block class.

---
+++ src/core/Logic/Blocks/Misc/ThinConnectingBlock.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ThinConnectingBlock.cs" company="VoxelGame">
+//     MIT License
+//	   For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using OpenToolkit.Mathematics;
+using VoxelGame.Core.Physics;
+
+namespace VoxelGame.Core.Logic.Blocks
+{
+    /// <summary>
+    /// A thin block that connects to other blocks.
+    /// Data bit usage: <c>--nesw</c>
+    /// </summary>
+    // n = connected north
+    // e = connected east
+    // s = connected south
+    public class ThinConnectingBlock : ConnectingBlock
+    {
+        public ThinConnectingBlock(string name, string namedId, string texture, string post, string extension) :
+            base(
+                name,
+                namedId,
+                texture,
+                post,
+                extension,
+                new BoundingBox(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0625f, 0.5f, 0.0625f)))
+        {
+        }
+    }
+}
