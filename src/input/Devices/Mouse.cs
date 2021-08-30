555244655e59aae6441b05031b7857ca0760c966
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-08-30 19:51:35 +0200

    Tweak mouse input smoothing.

--- src/input/Devices/Mouse.cs
+++ src/input/Devices/Mouse.cs
@@ -4,7 +4,6 @@
 // </copyright>
 // <author>pershingthesecond</author>
 
-using System;
 using OpenToolkit.Mathematics;
 
 namespace VoxelGame.Input.Devices
@@ -34,7 +33,7 @@ namespace VoxelGame.Input.Devices
             float yScale = 1f / input.Window.Size.Y;
 
             delta = Vector2.Multiply(delta, (-xScale, yScale)) * 1000;
-            delta = Vector2.Lerp(oldDelta, delta, 0.5f);
+            delta = Vector2.Lerp(oldDelta, delta, 0.7f);
 
             oldDelta = Delta;
             Delta = delta;
