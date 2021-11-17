3237cae2560ef4a692f12b024377e7ae832b1a6a
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-11-18 00:35:04 +0100

    Add command help pages.

--- src/client/Console/Commands/Help.cs
+++ src/client/Console/Commands/Help.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // <author>pershingthesecond</author>
 
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 
 namespace VoxelGame.Client.Console.Commands
@@ -13,19 +15,52 @@ namespace VoxelGame.Client.Console.Commands
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public class Help : Command
     {
-        private CommandInvoker commandInvoker;
+        private const int PageSize = 5;
+        private readonly CommandInvoker commandInvoker;
+
+        private List<List<string>> commandPages = new();
 
         public Help(CommandInvoker invoker)
         {
             commandInvoker = invoker;
+            commandInvoker.CommandsUpdated += BuildPages;
         }
 
         public override string Name => "help";
         public override string HelpText => "Provides help with using the commands.";
 
+        private void BuildPages()
+        {
+            List<string> commands = commandInvoker.CommandNames
+                .Select(command => $"{command} # {commandInvoker.GetCommandHelpText(command)}").ToList();
+
+            commandPages.Add(new List<string>());
+
+            foreach (string command in commands)
+            {
+                if (commandPages[^1].Count >= PageSize) commandPages.Add(new List<string>());
+
+                commandPages[^1].Add(command);
+            }
+        }
+
         public void Invoke()
         {
             Context.Console.WriteResponse("Use 'help' to get information on available commands.");
+            Context.Console.WriteResponse("Use 'help <page : int>' to get a specific command list page.");
+        }
+
+        public void Invoke(int page)
+        {
+            if (page > commandPages.Count || page <= 0)
+            {
+                Context.Console.WriteError($"There are only {commandPages.Count} pages of commands.");
+            }
+            else
+            {
+                Context.Console.WriteResponse($"Page {page} of {commandPages.Count}:");
+                commandPages[page - 1].ForEach(Context.Console.WriteResponse);
+            }
         }
     }
 }
