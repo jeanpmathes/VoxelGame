a8e1fef7b6fd687a051c16f2ee8aa60460046b8a
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-11-21 22:42:52 +0100

    Add teleport command.

---
+++ src/client/Console/Commands/Teleport.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Teleport.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using JetBrains.Annotations;
+
+namespace VoxelGame.Client.Console.Commands
+{
+    #pragma warning disable CA1822
+
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class Teleport : Command
+    {
+        public override string Name => "teleport";
+        public override string HelpText => "Teleport to a specified position.";
+
+        public void Invoke(float x, float y, float z)
+        {
+            Context.Player.Position = (x, y, z);
+        }
+    }
+}
