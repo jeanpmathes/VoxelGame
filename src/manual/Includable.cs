7c4d24a9f9e8241e8c3d1bc1ffb7f8b1db1ce36c
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-01-16 02:15:31 +0100

    Add manual sections

--- src/manual/Includable.cs
+++ src/manual/Includable.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // <author>pershingthesecond</author>
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace VoxelGame.Manual
@@ -14,6 +16,8 @@ namespace VoxelGame.Manual
     public class Includable
     {
         private readonly string outputPath;
+
+        private readonly List<Section> sections = new();
 
         /// <summary>
         ///     Create a new includable document.
@@ -26,11 +30,50 @@ namespace VoxelGame.Manual
         }
 
         /// <summary>
+        ///     Add a new section to this document.
+        /// </summary>
+        /// <param name="section">The section to add to the bottom.</param>
+        public void AddSection(Section section)
+        {
+            sections.Add(section);
+        }
+
+        /// <summary>
+        ///     Create a new section and add it to this document.
+        /// </summary>
+        /// <param name="title">The title of the section to add.</param>
+        /// <returns>The created section.</returns>
+        public Section CreateSection(string title)
+        {
+            var section = Section.Create(title);
+            AddSection(section);
+
+            return section;
+        }
+
+        /// <summary>
+        ///     Create and add multiple sections for a sequence of items.
+        /// </summary>
+        /// <param name="items">The items to add sections for.</param>
+        /// <param name="generator">A generator creating sections for items.</param>
+        /// <typeparam name="T">The item type.</typeparam>
+        public void CreateSections<T>(IEnumerable<T> items, Func<T, Section> generator)
+        {
+            foreach (T item in items) AddSection(generator(item));
+        }
+
+        /// <summary>
         ///     Generate the output file.
         /// </summary>
         public void Generate()
         {
-            using (FileStream outputStream = File.Create(outputPath)) {}
+            using StreamWriter writer = File.CreateText(outputPath);
+
+            foreach (Section section in sections)
+            {
+                section.Generate(writer);
+                writer.WriteLine();
+            }
         }
     }
 }
---
+++ src/manual/Section.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="Section.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using System.IO;
+
+namespace VoxelGame.Manual
+{
+    /// <summary>
+    ///     A section of a document, describing a topic.
+    /// </summary>
+    public class Section
+    {
+        private readonly string title;
+
+        private Section(string title)
+        {
+            this.title = title;
+        }
+
+        /// <summary>
+        ///     Create a new section.
+        /// </summary>
+        /// <param name="title">The title of the section.</param>
+        /// <returns>The created section.</returns>
+        public static Section Create(string title)
+        {
+            return new Section(title);
+        }
+
+        internal void Generate(StreamWriter writer)
+        {
+            writer.WriteLine(@$"\subsection{{{title}}}\label{{subsec:{title.ToLower()}}}");
+        }
+    }
+}
