2437dfa9e006424fc946a9c4c6ff4884ee980f5f
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-02-20 14:04:28 +0100

    Add assembly attributes to VoxelGame.Graphics

---
+++ src/graphics/Graphics.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="Graphics.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(isCompliant: false)]
+[assembly: ComVisible(visibility: false)]
