5b66e6b5fe78abc2828f1600971348bdcff8ce06
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-05-11 19:30:38 +0200

    Add two new types for chunk and section position

---
+++ src/core/Logic/ChunkPosition.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="ChunkPosition.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using System;
+
+namespace VoxelGame.Core.Logic;
+
+/// <summary>
+///     The position of a chunk in the world.
+/// </summary>
+public readonly struct ChunkPosition : IEquatable<ChunkPosition>
+{
+    /// <summary>
+    ///     Create a chunk position with the given coordinates.
+    /// </summary>
+    public ChunkPosition(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    /// <summary>
+    ///     The x coordinate.
+    /// </summary>
+    public int X { get; init; }
+
+    /// <summary>
+    ///     The y coordinate.
+    /// </summary>
+    public int Y { get; init; }
+
+    /// <summary>
+    ///     The z coordinate.
+    /// </summary>
+    public int Z { get; init; }
+
+    /// <summary>
+    ///     The equality comparison.
+    /// </summary>
+    public bool Equals(ChunkPosition other)
+    {
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return obj is ChunkPosition other && Equals(other);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
+    }
+
+    /// <summary>
+    ///     The equality operator.
+    /// </summary>
+    public static bool operator ==(ChunkPosition left, ChunkPosition right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <summary>
+    ///     The inequality operator.
+    /// </summary>
+    public static bool operator !=(ChunkPosition left, ChunkPosition right)
+    {
+        return !left.Equals(right);
+    }
+}
---
+++ src/core/Logic/SectionPosition.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="SectionPosition.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using System;
+
+namespace VoxelGame.Core.Logic;
+
+/// <summary>
+///     The position of a section in the world.
+/// </summary>
+public readonly struct SectionPosition : IEquatable<SectionPosition>
+{
+    /// <summary>
+    ///     Create a section position with the given coordinates.
+    /// </summary>
+    public SectionPosition(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    /// <summary>
+    ///     The x coordinate.
+    /// </summary>
+    public int X { get; init; }
+
+    /// <summary>
+    ///     The y coordinate.
+    /// </summary>
+    public int Y { get; init; }
+
+    /// <summary>
+    ///     The z coordinate.
+    /// </summary>
+    public int Z { get; init; }
+
+    /// <summary>
+    ///     The equality comparison.
+    /// </summary>
+    public bool Equals(SectionPosition other)
+    {
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return obj is SectionPosition other && Equals(other);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
+    }
+
+    /// <summary>
+    ///     The equality operator.
+    /// </summary>
+    public static bool operator ==(SectionPosition left, SectionPosition right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <summary>
+    ///     The inequality operator.
+    /// </summary>
+    public static bool operator !=(SectionPosition left, SectionPosition right)
+    {
+        return !left.Equals(right);
+    }
+}
