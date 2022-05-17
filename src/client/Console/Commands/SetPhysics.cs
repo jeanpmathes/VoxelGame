3eaf04b1a004e3daf8865edceab658e35e6cd440
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-05-17 19:07:32 +0200

    Add command to disable physics

---
+++ src/client/Console/Commands/SetPhysics.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="SetPhysics.cs" company="VoxelGame">
+//     MIT License
+//     For full license see the repository.
+// </copyright>
+// <author>pershingthesecond</author>
+
+using JetBrains.Annotations;
+
+namespace VoxelGame.Client.Console.Commands;
+    #pragma warning disable CA1822
+
+/// <summary>
+///     Allows to enable or disable physics for the player.
+/// </summary>
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class SetPhysics : Command
+{
+    /// <inheritdoc />
+    public override string Name => "set-physics";
+
+    /// <inheritdoc />
+    public override string HelpText => "Set whether physics calculations are performed for the player.";
+
+    /// <exclude />
+    public void Invoke(bool enabled)
+    {
+        Context.Player.DoPhysics = enabled;
+    }
+}
