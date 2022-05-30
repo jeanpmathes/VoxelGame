68b655517a8278a0f0bcdbc197580d706ea723a4
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-05-30 23:47:30 +0200

    Remove unneeded null handling

--- src/core/Logic/Fluid.cs
+++ src/core/Logic/Fluid.cs
@@ -185,87 +185,85 @@ public abstract partial class Fluid : IIdentifiable<uint>, IIdentifiable<string>
     /// <param name="context">The context of the meshing operation.</param>
     public void CreateMesh(Vector3i position, FluidMeshInfo info, MeshingContext context)
     {
-        if (RenderType != RenderType.NotRendered &&
-            (info.Block is IFillable { RenderFluid: true } ||
-             info.Block is not IFillable && !info.Block.IsSolidAndFull))
+        if (RenderType == RenderType.NotRendered || info.Block is not IFillable { RenderFluid: true } &&
+            (info.Block is IFillable || info.Block.IsSolidAndFull)) return;
+
+        VaryingHeightMeshFaceHolder[] fluidMeshFaceHolders =
+            context.GetFluidMeshFaceHolders(RenderType == RenderType.Opaque);
+
+        MeshFluidSide(BlockSide.Front);
+        MeshFluidSide(BlockSide.Back);
+        MeshFluidSide(BlockSide.Left);
+        MeshFluidSide(BlockSide.Right);
+        MeshFluidSide(BlockSide.Bottom);
+        MeshFluidSide(BlockSide.Top);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void MeshFluidSide(BlockSide side)
         {
-            VaryingHeightMeshFaceHolder[] fluidMeshFaceHolders =
-                context.GetFluidMeshFaceHolders(RenderType == RenderType.Opaque);
+            Fluid? fluidToCheck;
+            Block? blockToCheck;
 
-            MeshFluidSide(BlockSide.Front);
-            MeshFluidSide(BlockSide.Back);
-            MeshFluidSide(BlockSide.Left);
-            MeshFluidSide(BlockSide.Right);
-            MeshFluidSide(BlockSide.Bottom);
-            MeshFluidSide(BlockSide.Top);
+            (blockToCheck, fluidToCheck) = context.GetBlockAndFluid(
+                side.Offset(position),
+                side,
+                out int sideHeight);
 
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            void MeshFluidSide(BlockSide side)
-            {
-                Fluid? fluidToCheck;
-                Block? blockToCheck;
+            bool atVerticalEnd = side is BlockSide.Top or BlockSide.Bottom;
 
-                (blockToCheck, fluidToCheck) = context.GetBlockAndFluid(
-                    side.Offset(position),
-                    side,
-                    out int sideHeight);
+            bool isNeighborFluidMeshed =
+                blockToCheck is IFillable { RenderFluid: true };
 
-                bool atVerticalEnd = side is BlockSide.Top or BlockSide.Bottom;
+            if (fluidToCheck != this || !isNeighborFluidMeshed) sideHeight = -1;
 
-                bool isNeighborFluidMeshed =
-                    blockToCheck is IFillable { RenderFluid: true };
+            bool flowsTowardsFace = side == BlockSide.Top
+                ? Direction == VerticalFlow.Upwards
+                : Direction == VerticalFlow.Downwards;
 
-                if (fluidToCheck != this || !isNeighborFluidMeshed) sideHeight = -1;
+            bool meshAtNormal = (int) info.Level > sideHeight && !blockToCheck.IsOpaque;
 
-                bool flowsTowardsFace = side == BlockSide.Top
-                    ? Direction == VerticalFlow.Upwards
-                    : Direction == VerticalFlow.Downwards;
+            bool meshAtEnd =
+                flowsTowardsFace && sideHeight != 7 && !blockToCheck.IsOpaque
+                || !flowsTowardsFace && (info.Level != FluidLevel.Eight ||
+                                         fluidToCheck != this &&
+                                         blockToCheck?.IsOpaque != true);
 
-                bool meshAtNormal = (int) info.Level > sideHeight && blockToCheck?.IsOpaque != true;
+            if (atVerticalEnd ? !meshAtEnd : !meshAtNormal) return;
 
-                bool meshAtEnd =
-                    flowsTowardsFace && sideHeight != 7 && blockToCheck?.IsOpaque != true
-                    || !flowsTowardsFace && (info.Level != FluidLevel.Eight ||
-                                             fluidToCheck != this &&
-                                             blockToCheck?.IsOpaque != true);
+            FluidMeshData mesh = GetMeshData(info with { Side = side });
 
-                if (atVerticalEnd ? !meshAtEnd : !meshAtNormal) return;
+            bool singleSided = blockToCheck?.IsOpaque == false &&
+                               blockToCheck.IsSolidAndFull;
 
-                FluidMeshData mesh = GetMeshData(info with { Side = side });
+            (int x, int y, int z) = position;
+            side.Corners(out int[] a, out int[] b, out int[] c, out int[] d);
 
-                bool singleSided = blockToCheck?.IsOpaque == false &&
-                                   blockToCheck.IsSolidAndFull;
+            // int: uv-- ---- ---- ---- -xxx xxey yyyz zzzz (uv: texture coords; xyz: position; e: lower/upper end)
+            int upperDataA = (0 << 31) | (0 << 30) | ((x + a[0]) << 10) | (a[1] << 9) | (y << 5) |
+                             (z + a[2]);
 
-                (int x, int y, int z) = position;
-                side.Corners(out int[] a, out int[] b, out int[] c, out int[] d);
+            int upperDataB = (0 << 31) | (1 << 30) | ((x + b[0]) << 10) | (b[1] << 9) | (y << 5) |
+                             (z + b[2]);
 
-                // int: uv-- ---- ---- ---- -xxx xxey yyyz zzzz (uv: texture coords; xyz: position; e: lower/upper end)
-                int upperDataA = (0 << 31) | (0 << 30) | ((x + a[0]) << 10) | (a[1] << 9) | (y << 5) |
-                                 (z + a[2]);
+            int upperDataC = (1 << 31) | (1 << 30) | ((x + c[0]) << 10) | (c[1] << 9) | (y << 5) |
+                             (z + c[2]);
 
-                int upperDataB = (0 << 31) | (1 << 30) | ((x + b[0]) << 10) | (b[1] << 9) | (y << 5) |
-                                 (z + b[2]);
+            int upperDataD = (1 << 31) | (0 << 30) | ((x + d[0]) << 10) | (d[1] << 9) | (y << 5) |
+                             (z + d[2]);
 
-                int upperDataC = (1 << 31) | (1 << 30) | ((x + c[0]) << 10) | (c[1] << 9) | (y << 5) |
-                                 (z + c[2]);
+            // int: tttt tttt t--- -nnn hhhh dlll siii iiii (t: tint; n: normal; h: side height; d: direction; l: level; s: isStatic; i: texture index)
+            int lowerData = (mesh.Tint.GetBits(context.FluidTint) << 23) | ((int) side << 16) |
+                            ((sideHeight + 1) << 12) |
+                            (Direction.GetBit() << 11) | ((int) info.Level << 8) |
+                            (info.IsStatic ? 1 << 7 : 0 << 7) |
+                            ((((mesh.TextureIndex - 1) >> 4) + 1) & 0b0111_1111);
 
-                int upperDataD = (1 << 31) | (0 << 30) | ((x + d[0]) << 10) | (d[1] << 9) | (y << 5) |
-                                 (z + d[2]);
-
-                // int: tttt tttt t--- -nnn hhhh dlll siii iiii (t: tint; n: normal; h: side height; d: direction; l: level; s: isStatic; i: texture index)
-                int lowerData = (mesh.Tint.GetBits(context.FluidTint) << 23) | ((int) side << 16) |
-                                ((sideHeight + 1) << 12) |
-                                (Direction.GetBit() << 11) | ((int) info.Level << 8) |
-                                (info.IsStatic ? 1 << 7 : 0 << 7) |
-                                ((((mesh.TextureIndex - 1) >> 4) + 1) & 0b0111_1111);
-
-                fluidMeshFaceHolders[(int) side].AddFace(
-                    position,
-                    lowerData,
-                    (upperDataA, upperDataB, upperDataC, upperDataD),
-                    singleSided,
-                    info.Level == FluidLevel.Eight);
-            }
+            fluidMeshFaceHolders[(int) side].AddFace(
+                position,
+                lowerData,
+                (upperDataA, upperDataB, upperDataC, upperDataD),
+                singleSided,
+                info.Level == FluidLevel.Eight);
         }
     }
 
