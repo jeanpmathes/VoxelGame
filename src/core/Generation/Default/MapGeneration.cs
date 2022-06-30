4f2e08a13548208c62099cbed3b439506bed9466
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-06-30 19:54:13 +0200

    Build an adjacency structure for the generated pieces

--- src/core/Generation/Default/MapGeneration.cs
+++ src/core/Generation/Default/MapGeneration.cs
@@ -12,9 +12,11 @@ using System.IO;
 
 namespace VoxelGame.Core.Generation.Default;
 
+#pragma warning disable S4017
+
 public partial class Map
 {
-    private static void GenerateContinents(Data data, int seed)
+    private static List<List<int>> FillWithPieces(Data data, int seed)
     {
         FastNoiseLite noise = new(seed);
         noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
@@ -22,6 +24,8 @@ public partial class Map
 
         var currentPiece = 0;
         Dictionary<double, int> valueToPiece = new();
+
+        Dictionary<int, HashSet<int>> adjacencyHashed = new();
 
         for (var x = 0; x < Width; x++)
         for (var y = 0; y < Width; y++)
@@ -32,7 +36,60 @@ public partial class Map
             if (!valueToPiece.ContainsKey(value)) valueToPiece[value] = currentPiece++;
 
             current.continent = valueToPiece[value];
+
+            UpdateAdjacencies(data, adjacencyHashed, ref current, (x, y));
         }
+
+        return BuildAdjacencyList(adjacencyHashed);
+    }
+
+    private static void UpdateAdjacencies(Data data, IDictionary<int, HashSet<int>> adjacencyHashed, ref Cell current, (int, int) position)
+    {
+        void AddAdjacency(int a, int b)
+        {
+            if (!adjacencyHashed.ContainsKey(a))
+                adjacencyHashed[a] = new HashSet<int>();
+
+            if (!adjacencyHashed.ContainsKey(b))
+                adjacencyHashed[b] = new HashSet<int>();
+
+            adjacencyHashed[a].Add(b);
+            adjacencyHashed[b].Add(a);
+        }
+
+        (int x, int y) = position;
+
+        if (x != 0)
+        {
+            ref Cell left = ref data.GetCell(x - 1, y);
+
+            if (left.continent != current.continent) AddAdjacency(left.continent, current.continent);
+        }
+
+        if (y != 0)
+        {
+            ref Cell top = ref data.GetCell(x, y - 1);
+
+            if (top.continent != current.continent) AddAdjacency(top.continent, current.continent);
+        }
+    }
+
+    private static List<List<int>> BuildAdjacencyList(Dictionary<int, HashSet<int>> adjacencyHashed)
+    {
+        List<List<int>> adjacency = new();
+
+        for (var continent = 0; continent < adjacencyHashed.Count; continent++)
+        {
+            List<int> neighbors = new(adjacencyHashed[continent]);
+            adjacency.Add(neighbors);
+        }
+
+        return adjacency;
+    }
+
+    private static void GenerateContinents(Data data, int seed)
+    {
+        List<List<int>> adjacency = FillWithPieces(data, seed);
     }
 
     [Conditional("DEBUG")]
