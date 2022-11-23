57372242a2e62fa253d17aee635a321ea31fe27a
Jean Patrick Mathes <jeanpatrick.mathes@student.kit.edu>
2022-11-23 23:39:46 +0100

    Refactor tree generation using shapes

--- src/core/Logic/Structures/Tree.cs
+++ src/core/Logic/Structures/Tree.cs
@@ -5,6 +5,7 @@
 // <author>pershingthesecond</author>
 
 using System;
+using System.Diagnostics;
 using OpenTK.Mathematics;
 using VoxelGame.Core.Utilities;
 
@@ -53,11 +54,68 @@ public class Tree : DynamicStructure
 
     private readonly Kind kind;
 
+    private readonly (int height, float factor) needleConfig = (height: 8, factor: 0.35f);
+
+    private readonly Shape3D needleCrown = new Cone
+    {
+        Position = new Vector3(x: 2, y: 3, z: 2),
+        BottomRadius = 2.5f,
+        TopRadius = 0.0f,
+        Height = 9.0f
+    };
+
     private readonly Vector3i needleExtents = new(x: 5, y: 11, z: 5);
+
+    private readonly (int height, float factor) normal2Config = (height: 7, factor: 0.25f);
+
+    private readonly Shape3D normal2Crown = new Spheroid
+    {
+        Position = new Vector3(x: 2, y: 5.5f, z: 2),
+        Radius = new Vector3(x: 2.5f, y: 4.0f, z: 2.5f)
+    };
+
     private readonly Vector3i normal2Extents = new(x: 5, y: 9, z: 5);
+
+    private readonly (int height, float factor) normalConfig = (height: 7, factor: 0.25f);
+
+    private readonly Shape3D normalCrown = new Sphere
+    {
+        Position = new Vector3(x: 2, y: 6, z: 2),
+        Radius = 2.5f
+    };
+
     private readonly Vector3i normalExtents = new(x: 5, y: 9, z: 5);
+
+    private readonly (int height, float factor) palmConfig = (height: 9, factor: 0.25f);
+
+    private readonly Shape3D palmCrown = new Sphere
+    {
+        Position = new Vector3(x: 2, y: 9, z: 2),
+        Radius = 1.5f
+    };
+
     private readonly Vector3i palmExtents = new(x: 5, y: 11, z: 5);
+
+    private readonly (int height, float factor) savannaConfig = (height: 7, factor: 0.25f);
+
+    private readonly Shape3D savannaCrown = new Cone
+    {
+        Position = new Vector3(x: 2, y: 7, z: 2),
+        BottomRadius = 2.5f,
+        TopRadius = 2.5f,
+        Height = 1.0f
+    };
+
     private readonly Vector3i savannaExtents = new(x: 5, y: 8, z: 5);
+
+    private readonly (int height, float factor) tropicalConfig = (height: 14, factor: 0.25f);
+
+    private readonly Shape3D tropicalCrown = new Spheroid
+    {
+        Position = new Vector3(x: 4, y: 14, z: 4),
+        Radius = new Vector3(x: 4.5f, y: 1.5f, z: 4.5f)
+    };
+
     private readonly Vector3i tropicalExtents = new(x: 9, y: 16, z: 9);
 
     /// <summary>
@@ -81,191 +139,50 @@ public class Tree : DynamicStructure
         _ => throw new InvalidOperationException()
     };
 
-    /// <inheritdoc />
-    protected override (Content, bool)? GetContent(Vector3i offset)
+    private Shape3D GetCrownShape()
     {
         return kind switch
         {
-            Kind.Normal => GetNormalContent(offset),
-            Kind.Normal2 => GetNormal2Content(offset),
-            Kind.Tropical => GetTropicalContent(offset),
-            Kind.Needle => GetNeedleContent(offset),
-            Kind.Palm => GetPalmContent(offset),
-            Kind.Savanna => GetSavannaContent(offset),
+            Kind.Normal => normalCrown,
+            Kind.Normal2 => normal2Crown,
+            Kind.Tropical => tropicalCrown,
+            Kind.Needle => needleCrown,
+            Kind.Palm => palmCrown,
+            Kind.Savanna => savannaCrown,
             _ => throw new InvalidOperationException()
         };
     }
 
-    private (Content, bool overwrite)? GetNormalContent(Vector3i offset)
+    private (int height, float factor) GetConfig()
     {
-        const int center = 2;
-
-        if (offset is {X: center, Z: center} and {Y: 0})
-            return (new Content(Block.Roots), overwrite: true);
-
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 7})
-            return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
-
-        // The crown of this tree is a sphere.
-
-        Vector3i crownCenter = new(center, y: 6, center);
-
-        const float radiusSquared = 2.5f * 2.5f;
-        float distanceSquared = Vector3.DistanceSquared(offset, crownCenter);
-
-        if (distanceSquared > radiusSquared) return null;
-
-        float closeness = 1 - distanceSquared / radiusSquared;
-
-        if (closeness < 0.25f * Random.NextSingle()) return null;
-
-        return (new Content(Block.Leaves), overwrite: false);
+        return kind switch
+        {
+            Kind.Normal => normalConfig,
+            Kind.Normal2 => normal2Config,
+            Kind.Tropical => tropicalConfig,
+            Kind.Needle => needleConfig,
+            Kind.Palm => palmConfig,
+            Kind.Savanna => savannaConfig,
+            _ => throw new InvalidOperationException()
+        };
     }
 
-    private (Content, bool overwrite)? GetNormal2Content(Vector3i offset)
+    /// <inheritdoc />
+    protected override (Content content, bool overwrite)? GetContent(Vector3i offset)
     {
-        const int center = 2;
+        int center = Extents.X / 2;
+        Debug.Assert(Extents.X == Extents.Z);
 
-        if (offset is {X: center, Z: center} and {Y: 0})
+        (int height, float factor) = GetConfig();
+
+        if (offset.X == center && offset.Y == 0 && offset.Z == center)
             return (new Content(Block.Roots), overwrite: true);
 
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 7})
+        if (offset.X == center && offset.Z == center && offset.Y < height)
             return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
 
-        // The crown of this tree is a spheroid.
-
-        Vector3 crownCenter = new(center, y: 5.5f, center);
-
-        const float crownHeight = 4.0f;
-        const float crownRadius = 2.5f;
-
-        Vector3 point = offset - crownCenter;
-
-        float a = point.X * point.X / (crownRadius * crownRadius);
-        float b = point.Y * point.Y / (crownHeight * crownHeight);
-        float c = point.Z * point.Z / (crownRadius * crownRadius);
-
-        float closeness = 1 - (a + b + c);
-
-        if (closeness < 0.25f * Random.NextSingle()) return null;
-
-        return (new Content(Block.Leaves), overwrite: false);
-    }
-
-    private (Content, bool overwrite)? GetTropicalContent(Vector3i offset)
-    {
-        const int center = 4;
-
-        if (offset is {X: center, Z: center} and {Y: 0})
-            return (new Content(Block.Roots), overwrite: true);
-
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 14})
-            return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
-
-        // The crown of this tree is a spheroid.
-
-        Vector3i crownCenter = new(center, y: 14, center);
-
-        const float crownHeight = 1.5f;
-        const float crownRadius = 4.5f;
-
-        Vector3 point = offset - crownCenter;
-
-        float a = point.X * point.X / (crownRadius * crownRadius);
-        float b = point.Y * point.Y / (crownHeight * crownHeight);
-        float c = point.Z * point.Z / (crownRadius * crownRadius);
-
-        float closeness = 1 - (a + b + c);
-
-        if (closeness < 0.25f * Random.NextSingle()) return null;
-
-        return (new Content(Block.Leaves), overwrite: false);
-    }
-
-    private (Content, bool overwrite)? GetNeedleContent(Vector3i offset)
-    {
-        const int center = 2;
-
-        if (offset is {X: center, Z: center} and {Y: 0})
-            return (new Content(Block.Roots), overwrite: true);
-
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 8})
-            return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
-
-        // The crown of this tree is a cone.
-
-        Vector3 crownStart = new(center, y: 3, center);
-
-        const float crownHeight = 9.0f;
-        const float baseRadius = 2.5f;
-
-        Vector3 point = offset - crownStart;
-
-        float height = point.Y / crownHeight;
-        float radius = baseRadius * (1 - height);
-
-        if (height is < 0 or > 1) return null;
-
-        float radiusSquared = radius * radius;
-        float distanceSquared = point.X * point.X + point.Z * point.Z;
-
-        float closeness = 1 - distanceSquared / radiusSquared;
-
-        if (closeness < 0.35f * Random.NextSingle()) return null;
-
-        return (new Content(Block.Leaves), overwrite: false);
-    }
-
-    private (Content, bool overwrite)? GetPalmContent(Vector3i offset)
-    {
-        const int center = 2;
-
-        if (offset is {X: center, Z: center} and {Y: 0})
-            return (new Content(Block.Roots), overwrite: true);
-
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 9})
-            return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
-
-        // The crown of this tree is a small sphere.
-
-        Vector3i crownCenter = new(center, y: 9, center);
-
-        const float radiusSquared = 1.5f * 1.5f;
-        float distanceSquared = Vector3.DistanceSquared(offset, crownCenter);
-
-        if (distanceSquared > radiusSquared) return null;
-
-        float closeness = 1 - distanceSquared / radiusSquared;
-
-        if (closeness < 0.25f * Random.NextSingle()) return null;
-
-        return (new Content(Block.Leaves), overwrite: false);
-    }
-
-    private (Content, bool overwrite)? GetSavannaContent(Vector3i offset)
-    {
-        const int center = 2;
-
-        if (offset is {X: center, Z: center} and {Y: 0})
-            return (new Content(Block.Roots), overwrite: true);
-
-        if (offset is {X: center, Z: center} and {Y: > 0 and < 7})
-            return (new Content(Block.Specials.Log.GetInstance(Axis.Y), FluidInstance.Default), overwrite: true);
-
-        // The crown of this tree is a flat disk.
-
-        Vector3i crownPosition = new(center, y: 7, center);
-
-        if (offset.Y != crownPosition.Y) return null;
-
-        const float radiusSquared = 2.5f * 2.5f;
-        float distanceSquared = Vector3.DistanceSquared(offset, crownPosition);
-
-        if (distanceSquared > radiusSquared) return null;
-
-        float closeness = 1 - distanceSquared / radiusSquared;
-
-        if (closeness < 0.25f * Random.NextSingle()) return null;
+        if (!GetCrownShape().Contains(offset, out float closeness)) return null;
+        if (closeness < factor * Random.NextSingle()) return null;
 
         return (new Content(Block.Leaves), overwrite: false);
     }
