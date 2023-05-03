39aa6a10a95242f37db7346d65aa9105b36148ca
Jean Patrick Mathes <jean.mathes@student.kit.edu>
2023-05-03 17:44:47 +0200

    Use original clear/sky color

--- src/client/Resources/Shaders/Miss.hlsl
+++ src/client/Resources/Shaders/Miss.hlsl
@@ -3,9 +3,5 @@
 [shader("miss")]
 void Miss(inout HitInfo payload : SV_RayPayload)
 {
-    uint2 launchIndex = DispatchRaysIndex().xy;
-    float2 dims = float2(DispatchRaysDimensions().xy);
-
-    const float ramp = launchIndex.y / dims.y;
-    payload.colorAndDistance = float4(0.0f, 0.2f, 0.7f - 0.3f * ramp, -1.0f);
+    payload.colorAndDistance = float4(0.5f, 0.8f, 0.9f, -1.0f);
 }
