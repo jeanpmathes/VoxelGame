f8d6af323d521d0c5dfca0738cffc60a48f9b606
Jean P. Mathes <jeanpatrick.mathes@gmx.de>
2025-10-26 18:56:18 +1000

    Add error handling for any errors occuring in commands

--- src/client/Console/CommandInvoker.cs
+++ src/client/Console/CommandInvoker.cs
@@ -201,6 +201,14 @@ public sealed partial class CommandInvoker : IResource
         catch (TargetInvocationException e)
         {
             LogErrorInvokingCommand(logger, e.InnerException, method.Name);
+            
+            context.Output.WriteError($"Error while invoking command '{command.Name}', see log for details");
+        }
+        catch (Exception e)
+        {
+            LogErrorExecutingCommand(logger, e, command.Name);
+            
+            context.Output.WriteError($"Error while executing command '{command.Name}', see log for details");
         }
     }
 
@@ -231,6 +239,9 @@ public sealed partial class CommandInvoker : IResource
 
     [LoggerMessage(EventId = LogID.CommandInvoker + 7, Level = LogLevel.Error, Message = "Error while invoking command '{Command}'")]
     private static partial void LogErrorInvokingCommand(ILogger logger, Exception? exception, String command);
+    
+    [LoggerMessage(EventId = LogID.CommandInvoker + 8, Level = LogLevel.Error, Message = "Error while executing command '{Command}'")]
+    private static partial void LogErrorExecutingCommand(ILogger logger, Exception? exception, String command);
 
     #endregion LOGGING
 }
