589d03d2888a553747f9fb16d3111df02ba86a6e
Jean P. Mathes <jeanpatrick.mathes@gmx.de>
2025-11-12 11:49:12 +1000

    Remove unneccessary using

--- src/core/Visuals/Meshables/Meshing.cs
+++ src/core/Visuals/Meshables/Meshing.cs
@@ -6,7 +6,6 @@
 
 using System;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using OpenTK.Mathematics;
 using VoxelGame.Core.Utilities;
